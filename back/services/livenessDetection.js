9b52b0863dee64fae4e949d447a9216c65e2147d
technologymax2 <technologymax2@gmail.com>
2026-07-02 18:39:58 +0300

    Update livenessDetection.js

--- back/services/livenessDetection.js
+++ back/services/livenessDetection.js
@@ -1,10 +1,36 @@
-const checkLiveness = async (selfieImage) => {
-  console.log("Checking liveness:", selfieImage);
 
-  return {
-    live: true,
-    score: 0.99,
-  };
+const { spawn } = require("child_process");
+const path = require("path");
+
+const checkLiveness = (imagePath) => {
+  return new Promise((resolve, reject) => {
+
+    const script = path.join(__dirname, "../python/liveness.py");
+
+    const python = spawn("python", [
+      script,
+      path.join(__dirname, "..", imagePath),
+    ]);
+
+    let output = "";
+
+    python.stdout.on("data", (data) => {
+      output += data.toString();
+    });
+
+    python.stderr.on("data", (data) => {
+      console.error(data.toString());
+    });
+
+    python.on("close", () => {
+      try {
+        resolve(JSON.parse(output));
+      } catch (err) {
+        reject(err);
+      }
+    });
+
+  });
 };
 
 module.exports = checkLiveness;
