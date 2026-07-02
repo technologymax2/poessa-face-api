7fd062c70e0ea196dcee52f560621042a9c98800
technologymax2 <technologymax2@gmail.com>
2026-07-02 18:35:46 +0300

    Update faceRecognition.js

--- back/services/faceRecognition.js
+++ back/services/faceRecognition.js
@@ -1,19 +1,35 @@
-const { execFile } = require("child_process");
+const { spawn } = require("child_process");
 const path = require("path");
 
 const compareFaces = (registeredImage, selfieImage) => {
   return new Promise((resolve, reject) => {
+
     const script = path.join(__dirname, "../python/compare.py");
 
-    execFile(
-      "python",
-      [script, registeredImage, selfieImage],
-      (err, stdout) => {
-        if (err) return reject(err);
+    const python = spawn("python", [
+      script,
+      path.join(__dirname, "..", registeredImage),
+      path.join(__dirname, "..", selfieImage),
+    ]);
 
-        resolve(JSON.parse(stdout));
+    let data = "";
+
+    python.stdout.on("data", chunk => {
+      data += chunk.toString();
+    });
+
+    python.stderr.on("data", err => {
+      console.error(err.toString());
+    });
+
+    python.on("close", () => {
+      try {
+        resolve(JSON.parse(data));
+      } catch (err) {
+        reject(err);
       }
-    );
+    });
+
   });
 };
 
