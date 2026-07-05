27966f2f94f54338b834389813edb81ce8f3097a
technologymax2 <technologymax2@gmail.com>
2026-07-05 15:56:05 +0300

    Update Liveness.jsx

--- front/src/pages/Liveness.jsx
+++ front/src/pages/Liveness.jsx
@@ -1,1 +1,18 @@
 
+import { useLocation } from "react-router-dom";
+
+const Liveness = () => {
+  const { state } = useLocation();
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">
+        Liveness Detection
+      </h1>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Liveness;
