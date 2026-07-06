1536fb7cc6440aba0fed39fd24e2b09563ec1345
technologymax2 <technologymax2@gmail.com>
2026-07-06 16:41:48 +0300

    Update renewalRoutes.js

--- back/routes/renewalRoutes.js
+++ back/routes/renewalRoutes.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const {
   createRenewal,
   getCurrentRenewal,
+  updateRenewal,
+  deleteRenewal,
 } = require("../controllers/renewalController");
 
 router.post("/", createRenewal);
 
 router.get("/current", getCurrentRenewal);
+
+// 👇 እነዚህን 2 መስመሮች ጨምር
 router.put("/:id", updateRenewal);
 
 router.delete("/:id", deleteRenewal);
