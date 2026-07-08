f22aae5e1beed871a1252300f007a7858c023acd
technologymax2 <technologymax2@gmail.com>
2026-07-08 18:00:59 +0300

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; // Link እዚህ ጋር ተጨምሯል
-import VideoVerification from "./pages/OfficerDashboard";
+import OfficerCallCenter from "./pages/OfficerDashboard";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
